269af74ab9b56bc0eb1dec6aed3a85c64a965e88
LiamAndresEvans <liamandresevans@gmail.com>
2021-12-21 19:17:01 -0500

    poner puerto en app.js y creacion archivo procfile

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 const express = require('express');
 //para usar las rutas estaticas
 const path = require('path'); 
-
 const app = express();
+
+//variable para subir a heroku
+const PORT = process.env.PORT || 3000;
 
 //rutas estaticas
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) ); 
 //escuchar navegador al servidor express
-app.listen(3000, ()=> console.log("corriendo servidor con Express") );  
-
+app.listen( PORT, ()=> console.log(`corriendo servidor con Express en el puerto ${PORT}`) );
+//app.listen(process.env.PORT || 3000, ()=> console.log("corriendo servidor con Express") );  
+//app.listen(process.env.PORT || 3000, ()=> console.log("corriendo servidor con Express") );
 //app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname ,'./views/home.html')));
 //app.get('/', (req, res) => res.sendFile(path.resolve(__dirname ,'./views/index.html')));
